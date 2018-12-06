b6f5a81a70a9f20c6e2719cd646192d56da8198e
SaintlyVi <wiebke.toussaint@gmail.com>
2018-12-06 13:24:58 +0200

    renamed dlrprocessing > src and add data_files

--- setup.py
+++ setup.py
@@ -6,24 +6,32 @@ Created on Mon Mar 19 14:17:10 2018
 @author: SaintlyVi
 """
 from setuptools import setup, find_packages
+import os
+from src.support import usr_dir
 
 setup(
       name='dlrprocessing',
       version=0.1,
       description='Processes local DLR data',
-      long_description='This module helps researchers retrieve cleaned load profile\
-      and survey data from a local directory structure of the Domestic Load Research\
-      database. Processed data is store on disk.',
+      long_description='This module helps researchers process and clean load profile\
+      and survey data of the Domestic Load Research database from a local directory\
+      structure. Processed data is stored on disk.',
       keywords='domestic load research south africa data processing',
       url='https://github.com/wiebket/dlrprocessing',
       author='Wiebke Toussaint',
       author_email='wiebke.toussaint@gmail.com',
-      packages=find_packages(),
       license='CC-BY-NC',
       install_requires=['pandas','numpy','pyodbc','feather-format','plotly', 
                         'pathlib','pyshp','shapely'],#, 'optparse', 'json','os','glob' ],
+      packages=['dlrprocessing'],
+      package_dir = {'dlrprocessing': 'src'},
+#      package_data={'dlrprocessing':['data/geometa/site_geo.csv']}, -- not needed if specified in MANIFEST.in
+      data_files=[(os.path.join(usr_dir,'specs'), [os.path.join(
+                  'src','data','specs', f) for f in [files for root, dirs, files 
+                    in os.walk(os.path.join('src','data','specs'))][0]])],
       include_package_data=True,
       entry_points = {
-			'console_scripts': ['dlr_process_profiles=dlrprocessing.command_line:process_profiles', 'dlr_process_surveys=dlrprocessing.command_line:process_surveys'],
-    		}
+			'console_scripts': ['dlr_process_profiles=dlrprocessing.command_line:process_profiles',
+                       'dlr_process_surveys=dlrprocessing.command_line:process_surveys'],
+                       }
       )
