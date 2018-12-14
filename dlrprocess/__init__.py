756c301c5160f369ea1ada0e292cf917fb4c6730
Wiebke Toussaint <wiebke.toussaint@gmail.com>
2018-12-14 10:10:20 +0200

    Update __init__.py with new dir name

--- dlrprocess/__init__.py
+++ dlrprocess/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from dlrprocessing.loadprofiles import loadRawProfiles, reduceRawProfiles, loadReducedProfiles, genX
-from dlrprocessing.surveys import searchQuestions, searchAnswers, genS
-from dlrprocessing.plotprofiles import nanAnalysis
+from dlrprocess.loadprofiles import loadRawProfiles, reduceRawProfiles, loadReducedProfiles, genX
+from dlrprocess.surveys import searchQuestions, searchAnswers, genS
+from dlrprocesss.plotprofiles import nanAnalysis
